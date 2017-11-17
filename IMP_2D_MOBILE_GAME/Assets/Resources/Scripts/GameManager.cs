23528315d791ed6317c7b17a664c1d2e5907b7cb
Almondicus <tim.mandelbaum@haw-hamburg.de>
2017-11-17 17:52:58 +0100

    Created GameManager script
    Created:
    => GameManager
    => GameManager Prefab
    => LoaderScript
    
    GAMEMANAGER:
    => Initializes the BoardManager as boardScript and sets up the scenes
    
    => SINGLETON
    - the gamemanger is defined as a Singleton via public static instance => for preventing multiple launch
    
    => AWAKE
    - the function checks for the instance if the GameManager and destroys the gameObjects, if another GameObject with the instance exists
    - the function also prevents the destruction of the gameObject with DontDestroyOnLoad to keep track between multiple scenes => otherwise all GameObjects inside the Hierarchy get destroyed.
    - the boardScript get referenced to the BoardManager-script
    
    => INITGAME
    - InitGame() contains boardScript.SetupScene(level) => the method generates the scene for a specific level.
    
    GAMEMANAGER PREFAB:
    => created a GameManager prefab
    - "locked" the GameManager inside the Inspector
    - inserted GameObjects:
    => FloorTiles: Prefabs: Floor1...8
    => WallTiles: Prefabs: Wall1...8
    => FoodTiles: Prefabs: Food/Potion
    => EnemyTiles: Prefabs: Enemy01/02
    => OuterWallTiles: Prefabs: OuterWalls1...3
    - "unlocked" GameManager inside the Inspector
    - adjusted MainCamera: Position: X/Y/Z: 3,5/3,5/-10, Background: black/#000000
    
    LOADERSCRIPT:
    => added GameManager to the Prefabs
    => deleted GameManager from the Hierarchy
    => added LoaderScript to the MainCamera
    => added GameManager-Prefab to the "GameManager" of the LoaderScript

--- IMP_2D_MOBILE_GAME/Assets/Resources/Scripts/GameManager.cs
+++ IMP_2D_MOBILE_GAME/Assets/Resources/Scripts/GameManager.cs
@@ -4,13 +4,42 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    // DEFINING GAMEMANGER AS SIGLETON => PREVENTING MULTIPLE LAUNCH
+    public static GameManager instance = null;
+
+    // CREATING THE VARIABLE BOARDSCRIPT OF THE TYPE BOARDMANAGER
+    public BoardManager boardScript;
+
+    // DEBUG LVL 3 => WHERE ENEMIES FIRST APPEAR
+    private int level = 4;
+
 	// Use this for initialization
-	void Start () {
+	void Awake(){
+
+        // CHECK IF INSTANCE == NULL => ASSIGNE TO THIS
+        if (instance == null) {
+            instance = this;
+        }
+        // DESTROY INSTANCE => PREVENTING MULTIPLE GAMEMANAGER OBJECTS
+        else if(instance != this){
+            Destroy(gameObject);
+        }
+
+        // DON'T DESTROY ON LOAD
+        // WHEN LOADING A NEW SCENEN, NORMALLY ALL GAMEOBJECTS INSIDE THE HIRARCHY ARE DESTROYED
+        // THE GAMEMANAGER IS USED TO KEEP TRACK OF THE SCORE AND SO ON BETWEEN THE SCENES
+        // => DONTDESTROYONLOAD PRESERVES IT FROM BEING DESTROYED
+        DontDestroyOnLoad(gameObject);
+
+        // COMPONENT REFERENCE TO BOARDMANAGER SCRIPT
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void InitGame(){
+        // CALL THE SETUPSCENE FUNCTION OF THE BOARDSCRIPT VARIABLE => LEVEL AS ARGUMENT
+        boardScript.SetupScene(level);
+    }
+	 
 }
---
+++ IMP_2D_MOBILE_GAME/Assets/Resources/Scripts/Loader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+
+    // CHECK IF GAMEMANAGER HAS BEEN INSTANTIATED, IF NOT => INSTANTIATE ONE FROM THE PREFAB
+
+    public GameObject gameManager;
+
+	// Use this for initialization
+	void Start () {
+
+        // CHECK THE GAMEMANAGER STATIC VARIABLE FROM THE GAMEMANAGER SCRIPT
+        // IF == NULL => INSTANTIATE A NEW GAMEMANAGER => INSTANTIATE(GAMEMANAGER)
+        if (GameManager.instance == null){
+            Instantiate(gameManager);
+        }
+	}
+	 
+}
