504d34a303ffb413388749163962b260f2e7e51f
Ellyn737 <e.reese@mail.de>
2017-11-20 15:14:59 +0100

    adjustment of comments

--- IMP_2D_MOBILE_GAME/Assets/Resources/Scripts/Player.cs
+++ IMP_2D_MOBILE_GAME/Assets/Resources/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MovingObject {
 
 	//Damage the player can apply on the wall by chopping
 	public int wallDamage = 1;
-	//Health points per Flame
+	//Health points per Food and Soda
 	public int pointsPerFood = 10;
 	public int pointsPerSoda = 20;
 
@@ -40,19 +40,24 @@ public class Player : MovingObject {
 
 	// Update is called once per frame
 	void Update () {
+		//ignore the input, if it#s not the players turn
 		if (!GameManager.instance.playersTurn)
 			return;
 
+		//declare variables horizontal and vertical
 		int horizontal = 0;
 		int vertical = 0;
 
+		//initialize them with the keyboard input
 		horizontal = (int) Input.GetAxisRaw ("Horizontal");
 		vertical = (int)Input.GetAxisRaw ("Vertical");
 
+		//if you walk horizontally you cannot move vertically 
 		if (horizontal != 0)
 			vertical = 0;
-
+		//if you move horizontally or vertically
 		if (horizontal != 0 || vertical != 0)
+			//start AttemptMove and give it Wall as a component
 			AttemptMove<Wall> (horizontal, vertical);
 	}
 
@@ -70,31 +75,43 @@ public class Player : MovingObject {
 
 	}
 
-
+	//entering a trigger
 	private void OnTriggerEnter2D (Collider2D other){
+		//on exit
 		if (other.tag == "Exit") {
+			//restart the level --> GameManager builds a new one
 			Invoke ("Restart", restartLevelDelay);
+			//disable the player
 			enabled = false;
+			//on food
 		} else if (other.tag == "Food") {
+			//raise the food points by 10
 			food += pointsPerFood;
+			//disable the food --> vanishes
 			other.gameObject.SetActive (false);
+			//on soda
 		} else if (other.tag == "Soda") {
+			//raise the food points by 20
 			food += pointsPerSoda;
+			//disable the soda --> vanishes
 			other.gameObject.SetActive (false);
 		}
 			
 	}
 
 
+	//if there is an obstacle
 	protected override void OnCantMove<T>(T component){
 		Wall hitWall = component as Wall;
+		//activate the damage wall function
 		hitWall.DamageWall (wallDamage);
+		//set the animation for chopping the wall down
 		animator.SetTrigger ("playerChop");
 	}
 
 
 	private void Restart(){
-		//On restart load the level again
+		//On restart load the level again (automatically new level)
 		SceneManager.LoadScene ("Main");
 
 	}
