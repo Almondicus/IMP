d9b484bb4e0d0b62751eb63f40a4b80aede69423
Almondicus <tim.mandelbaum@haw-hamburg.de>
2017-11-29 16:28:23 +0100

    Created Wall script
    Created:
    => Wall script
    => configurated Sprites
    
    WALL
    => controls wall behaviour when attacked by Player
    
    DMGSPRITE
    - Sprite displayed after Player hit the wall
    
    HP
    - hit points
    
    DAMAGEWALL
    - function to change the sprite via the spriteRendere
    - subtracts loss from the current walls total hp
    - disables gameObject
    
     CONFIGURED SPRITES
    - added Wall script to Wall1...8 Prefabs
    - added the relevant floor tile sprite to the respective DmgSprite

---
+++ IMP_2D_MOBILE_GAME/Assets/Resources/Scripts/Wall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+    // PUBLIC VARIABLES
+    public Sprite dmgSprite; // SPRITE DISPLAYED AFTER PLAYER HIT THE WALL
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
