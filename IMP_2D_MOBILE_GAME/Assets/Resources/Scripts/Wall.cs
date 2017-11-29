204798fe2f04c721fe4fbb388d51668a6fd03600
Almondicus <tim.mandelbaum@haw-hamburg.de>
2017-11-29 17:21:03 +0100

    Edited Player Animator Controller
    Edited:
    => Player Animator Controller
    => Added Trigger for state change
    
    PLAYER ANIMATOR CONTROLLER
    - moved Player back to Hierarchy for testing
    - added Trigger: playerChop & playerHit
    - Made Transisions for:
    
    => PlayerChop - to PlayerIdle
    - ExitTime: 1
    - Transition Duration: 0
    
    => PlayerIdle
    - Has Exit Time: false -> no waiting time for animation to finish
    - Settings: Transition Animation: 0
    - Conditions: playerChop Trigger
    
    => PlayerHit
    - Has Exit Time: false
    - Transition Duration: 0
    - Conditions: playerHit Trigger

--- IMP_2D_MOBILE_GAME/Assets/Resources/Scripts/Wall.cs
+++ IMP_2D_MOBILE_GAME/Assets/Resources/Scripts/Wall.cs
@@ -5,15 +5,36 @@ using UnityEngine;
 public class Wall : MonoBehaviour {
 
     // PUBLIC VARIABLES
-    public Sprite dmgSprite; // SPRITE DISPLAYED AFTER PLAYER HIT THE WALL
+    // SPRITE DISPLAYED AFTER PLAYER HIT THE WALL
+    public Sprite dmgSprite;
+    // HIT POINTS
+    public int hp = 4;
 
-	// Use this for initialization
-	void Start () {
-		
+    // PRIVATE VARIABLES
+    private SpriteRenderer spriteRenderer;
+
+    // AWAKE FUNCTION
+    void Awake () {
+
+        // GET AND STORE COMPONENT REFERENCE TO OUR SPRITERENDERER
+        spriteRenderer = GetComponent<SpriteRenderer>(); 
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void DamageWall(int loss){
+
+        // CHANGE TO DAMAGED SPRITE
+        spriteRenderer.sprite = dmgSprite;
+
+        // SUBSTRACT LOSS FROM WALLS CURRENT HP TOTAL
+        hp -= loss;
+
+        // CHECK IF WALL HP <= 0
+        if(hp <= 0){
+            // DISABLE WALL GAME OBJECT
+            gameObject.SetActive(false);
+        } 
+
+    }
+	 
 }
